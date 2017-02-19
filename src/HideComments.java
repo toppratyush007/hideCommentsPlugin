f655a5a6114caf6ffbccacb4d71280da8300b031
pratyush <toppratyush007@gmail.com>
2017-02-19 13:06:13 +0530

    scratch behaviour of reading and replacing by reading content

---
+++ src/HideComments.java
@@ -0,0 +1,68 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+
+import java.io.*;
+
+/**
+ * Created by pratyush.k on 19/02/17.
+ */
+public class HideComments extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        final Project project = e.getProject();
+        if (project == null) {
+            return;
+        }
+        Editor editor = FileEditorManager.getInstance(project).getSelectedTextEditor();
+        if (editor == null) {
+            return;
+        }
+        final Document document = editor.getDocument();
+        if (document == null) {
+            return;
+        }
+        VirtualFile virtualFile = FileDocumentManager.getInstance().getFile(document);
+        if (virtualFile == null) {
+            return;
+        }
+        final String contents;
+        try {
+            BufferedReader br = new BufferedReader(new StringReader(document.getText()));
+            String currentLine;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((currentLine = br.readLine()) != null) {
+                stringBuilder.append(currentLine);
+                stringBuilder.append("\n");
+            }
+            contents = stringBuilder.toString();
+        } catch (IOException e1) {
+            return;
+        }
+        final Runnable readRunner = new Runnable() {
+            @Override
+            public void run() {
+                document.setText(contents);
+            }
+        };
+        ApplicationManager.getApplication().invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                CommandProcessor.getInstance().executeCommand(project, new Runnable() {
+                    @Override
+                    public void run() {
+                        ApplicationManager.getApplication().runWriteAction(readRunner);
+                    }
+                }, "DiskRead", null);
+            }
+        });
+    }
+}
